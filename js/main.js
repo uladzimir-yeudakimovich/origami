70a7ecc398bcb96ae45b02892575970b309592db
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-13 19:12:19 +0300

    bug fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 for (let i=0; i<100; i++) {
   setTimeout(function() {
-    document.getElementById('banner').style.opacity = 0.8 - i/100;
+    document.getElementById('banner').style.opacity = 1 - i/100;
   }, 50 + i*50);
 }
