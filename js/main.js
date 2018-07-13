7f6af6cd78234f55ba839a78bc348d1875adf42c
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-13 17:06:11 +0300

    updated js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
-setTimeout(function() {
-  document.getElementById('banner').style.opacity = "0.8";
-}, 1000);
-setTimeout(function() {
-  document.getElementById('banner').style.opacity = "0.6";
-}, 2000);
-setTimeout(function() {
-  document.getElementById('banner').style.opacity = "0.4";
-}, 3000);
-setTimeout(function() {
-  document.getElementById('banner').style.opacity = "0.2";
-}, 4000);
-setTimeout(function() {
-  document.getElementById('banner').style.opacity = "0";
-}, 5000);
+for (let i=0; i<100; i++) {
+  setTimeout(function() {
+    document.getElementById('banner').style.opacity = 0.8 - i/100;
+  }, 50 + i*50);
+}
