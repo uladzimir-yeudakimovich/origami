f37c7fdb18ba47a93f480bb9ea2a207f6fe4928b
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-13 16:57:50 +0300

    add js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+setTimeout(function() {
+  document.getElementById('banner').style.opacity = "0.8";
+}, 1000);
+setTimeout(function() {
+  document.getElementById('banner').style.opacity = "0.6";
+}, 2000);
+setTimeout(function() {
+  document.getElementById('banner').style.opacity = "0.4";
+}, 3000);
+setTimeout(function() {
+  document.getElementById('banner').style.opacity = "0.2";
+}, 4000);
+setTimeout(function() {
+  document.getElementById('banner').style.opacity = "0";
+}, 5000);
